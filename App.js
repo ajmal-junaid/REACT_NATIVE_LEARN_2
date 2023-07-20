10aacf0bc574c3e20310cc92468e1b026f35cb16
Ajmal junaid <ajmal6201@gmail.com>
2023-07-20 23:15:41 +0530

    nested view added

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import { View } from "react-native";
 
-export default function App(){
-  return <View style={{backgroundColor: "blue",flex: 1}}></View>
+export default function App() {
+  return (
+    <View style={{ backgroundColor: "blue", flex: 1 }}>
+      <View style={{ width: 200, height: 200, backgroundColor: "plum" }}></View>
+      <View style={{ width: 200, height: 200, backgroundColor: "green " }}></View>
+    </View>
+  );
 }
