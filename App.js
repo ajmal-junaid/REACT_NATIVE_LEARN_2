5bc8471797d08cca6e8c2711e3ddb4caae9bca73
Ajmal junaid <ajmal6201@gmail.com>
2023-07-25 10:54:51 +0530

    background image

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { Image, Text, View } from "react-native";
+import { Image, ImageBackground, Text, View } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png")
 
 export default function App() {
   return (
     <View style={{ backgroundColor: "plum", flex: 1, padding: 60 }}>
-      <Image source={logoImg} style={{width:300, height: 400}}/>
-      <Image source={{uri:"https://picsum.photos/300"}} style={{width:300, height: 400}}/>
+      {/* <Image source={logoImg} style={{width:300, height: 400}}/>
+      <Image source={{uri:"https://picsum.photos/300"}} style={{width:300, height: 400}}/> */}
+      <ImageBackground source={logoImg}>
+        <Text>Image text test</Text>
+      </ImageBackground>
     </View>
   );
 }
