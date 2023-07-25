2a022dd0056ada6893819b00b70d6fd03d8bef67
Ajmal junaid <ajmal6201@gmail.com>
2023-07-25 10:52:44 +0530

    static and network images

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png")
 
 export default function App() {
   return (
     <View style={{ backgroundColor: "plum", flex: 1, padding: 60 }}>
-      <Text><Text style={{color: "white"}}>Hello</Text> World</Text>
+      <Image source={logoImg} style={{width:300, height: 400}}/>
+      <Image source={{uri:"https://picsum.photos/300"}} style={{width:300, height: 400}}/>
     </View>
   );
 }
