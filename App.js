0cf597a4d7a5046cd52d2fdbb2f741625379406f
Ajmal junaid <ajmal6201@gmail.com>
2023-07-25 11:15:05 +0530

    onPress event

--- App.js
+++ App.js
@@ -1,39 +1,13 @@
+import { Button } from "react-native";
 import { ScrollView } from "react-native";
-import { Image, ImageBackground, Text, View } from "react-native";
+import { View } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={{ backgroundColor: "plum", flex: 1, padding: 50 }}>
       <ScrollView>
-        <Image
-          source={{ uri: "https://picsum.photos/300" }}
-          style={{ width: 300, height: 400 }}
-        />
-        <Text>
-          What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
-          and typesetting industry. Lorem Ipsum has been the industry's standard
-          dummy text ever since the 1500s, when an unknown printer took a galley
-          of type and scrambled it to make a type specimen book. It has survived
-          not only five centuries, but also the leap into electronic
-          typesetting, remaining essentially unchanged. It was popularised in
-          the 1960s with the release of Letraset sheets containing Lorem Ipsum
-          passages, and more recently with desktop publishing software like
-          Aldus PageMaker including versions of Lorem Ipsum. Why do we use it?
-          It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout. The point of
-          using Lorem Ipsum is that it has a more-or-less normal distribution of
-          letters, as opposed to using 'Content here, content here', making it
-          look like readable English. Many desktop publishing packages and web
-          page editors now use Lorem Ipsum as their default model text, and a
-          search for 'lorem ipsum' will uncover many web sites still in their
-          infancy. Various versions have evolved over the years, sometimes by
-          accident, sometimes on purpose (injected humour and the like).
-        </Text>
-        <Image
-          source={{ uri: "https://picsum.photos/300" }}
-          style={{ width: 300, height: 400 }}
-        />
+        <Button title="Press here" onPress={() => console.log("button pressed")}/>
       </ScrollView>
     </View>
   );
