d41ebac31029fb8c7014538c28510d8039425eaf
Ajmal junaid <ajmal6201@gmail.com>
2023-07-27 20:49:54 +0530

    modal with some props'

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
+import { Modal, Text } from "react-native";
+import { Button } from "react-native";
 import { View } from "react-native";
 
 export default function App() {
+  const [isModal, setIsModal] = useState(false);
   return (
-    <View style={{ backgroundColor: "blue", flex: 1 }}>
-      <View style={{ width: 200, height: 200, backgroundColor: "plum" }}></View>
-      <View style={{ width: 200, height: 200, backgroundColor: "green " }}></View>
+    <View style={{ backgroundColor: "blue", flex: 1, paddingTop: 30 }}>
+      <Button title="Press here" onPress={() => setIsModal(!isModal)} />
+      <Modal visible={isModal} onRequestClose={() => setIsModal(false)} animationType="slide" presentationStyle="pageSheet">
+        <View style={{ flex: 1, backgroundColor: "white", padding: 50 }}>
+          <Text>Modal content ok</Text>
+          <Button onPress={() => setIsModal(!isModal)} title="close modal" color="green" />
+        </View>
+      </Modal>
     </View>
   );
 }
