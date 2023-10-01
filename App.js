9f3b25423d5275e61adf8ce4821f261e22742dc9
Ajmal junaid <ajmal6201@gmail.com>
2023-10-01 13:28:55 +0530

    completed

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { View } from "react-native";
+import Greet from "./components/greet";
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: "blue", flex: 1 ,padding: 60}}>
-      <View style={{ width: 200, height: 200, backgroundColor: "plum" }}></View>
-      <View style={{ width: 200, height: 200, backgroundColor: "green " }}></View>
+    <View style={{ backgroundColor: "plum", flex: 1 ,padding: 60}}>
+      <Greet name="Ajmal junaid"/>
+      <Greet name="Clara"/>
     </View>
   );
 }
