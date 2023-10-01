58b36efae1226bd2a86dc2a0a21449a6a7e5dd87
Ajmal junaid <ajmal6201@gmail.com>
2023-10-01 13:10:50 +0530

    done all props

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import { View } from "react-native";
+import { useState } from "react";
+import { ActivityIndicator, Button, View } from "react-native";
 
 export default function App() {
+  const [isLoading,setIsLoading]= useState(false)
   return (
-    <View style={{ backgroundColor: "blue", flex: 1 }}>
-      <View style={{ width: 200, height: 200, backgroundColor: "plum" }}></View>
-      <View style={{ width: 200, height: 200, backgroundColor: "green " }}></View>
+    <View style={{ backgroundColor: "plum", flex: 1, padding: 60 }}>
+      <Button title="Press here"  onPress={()=>setIsLoading(!isLoading)}/>
+      <ActivityIndicator/>
+      <ActivityIndicator size="large"/>
+      <ActivityIndicator size="large" color="midnightblue"/>
+      <ActivityIndicator size="large" color="midnightblue" animating={isLoading}/>
+
     </View>
   );
 }
