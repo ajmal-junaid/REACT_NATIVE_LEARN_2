f7176b878c2b84ca4850e6c4325f301ec199e7bf
Ajmal junaid <ajmal6201@gmail.com>
2023-10-01 13:11:26 +0530

    padding added

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: "blue", flex: 1 }}>
+    <View style={{ backgroundColor: "blue", flex: 1 ,padding: 60}}>
       <View style={{ width: 200, height: 200, backgroundColor: "plum" }}></View>
       <View style={{ width: 200, height: 200, backgroundColor: "green " }}></View>
     </View>
