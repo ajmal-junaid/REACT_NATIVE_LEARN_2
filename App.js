6ac94ce7b57628364f13bc62c18c9368253b73cb
Ajmal junaid <ajmal6201@gmail.com>
2023-10-01 12:32:31 +0530

    status bar

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import { View } from "react-native";
+import { useState } from "react";
+import { Button, Modal, Text, View } from "react-native";
 
 export default function App() {
+  const [isModalVisible,setIsModalVisible] = useState(false)
   return (
-    <View style={{ backgroundColor: "blue", flex: 1 }}>
-      <View style={{ width: 200, height: 200, backgroundColor: "plum" }}></View>
-      <View style={{ width: 200, height: 200, backgroundColor: "green " }}></View>
+    <View style={{ backgroundColor: "yellow", flex: 1 , padding:60}}>
+      <Button
+      title="Press"
+      onPress={()=> setIsModalVisible(true)}
+      color="midnightblue"
+      />
+      <Modal
+        visible={isModalVisible}
+        onRequestClose={()=> setIsModalVisible(false)}
+        animationType="slide"
+        presentationStyle="pageSheet" //ios only
+      >
+        <View style ={{flex:1, backgroundColor: "lightblue",padding: 60}}>
+          <Text>Modal content</Text>
+          <Button 
+          title="Close"
+          color="midnightblue"
+          onPress={()=> setIsModalVisible(false)}
+          />
+        </View>
+
+      </Modal>
     </View>
   );
 }
+ 
